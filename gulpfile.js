e77f9dfaf8ac6f537882daaa29c37dad00fc1f45
Zzz <zkmd2012@inbox.ru>
2021-12-28 20:01:02 +0100

    Watch all changes in files and restart gulp command automatically

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,13 @@ global.app = {
 //tasks import
 import {copy} from './gulp/tasks/copy.js'
 
+//наблюдатель за изменениями в файлах
+function watcher() {
+    gulp.watch(path.watch.files, copy)
+}
+
+//Построение сценариев выполнения задач
+const dev = gulp.series(copy, watcher)
+
 //Выполнение сценария по умолчанию
-gulp.task('default', copy)
+gulp.task('default', dev)
