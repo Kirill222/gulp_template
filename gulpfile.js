d005b87a663f1a85cd9dbd0774e51b9bd08b7fd9
Zzz <zkmd2012@inbox.ru>
2021-12-30 00:00:10 +0100

    svg sprites generation added

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ import {scss} from './gulp/tasks/scss.js'
 import {js} from './gulp/tasks/js.js'
 import {images} from './gulp/tasks/images.js'
 import {otfToTtf, ttfToWoff, fontsStyle} from './gulp/tasks/fonts.js'
+import {svg} from './gulp/tasks/svg.js'
 
 //наблюдатель за изменениями в файлах
 function watcher() {
@@ -28,6 +29,8 @@ function watcher() {
     gulp.watch(path.watch.js, js)
     gulp.watch(path.watch.images, images)
 }
+
+export {svg}
 
 //последовательная обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
